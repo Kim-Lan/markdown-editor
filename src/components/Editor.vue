b4817c2d891f7e496f55551254755d0a523e2398
Kim-Lan <me@kim-lan.com>
2025-01-29 18:53:13 -0800

    Set up markdown rendering with markdown-it

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -2,7 +2,16 @@
 import { ref } from 'vue'
 import { markdown } from '../stores/markdown'
 
+defineEmits(['cursorChange']);
+
 const markdownInput = ref(null);
+
+function setCursorPosition() {
+  if (textarea.value) {
+    let position = textarea.value.selectionStart;
+    emit('cursorChange', position)
+  }
+}
 </script>
 
 <template>
@@ -11,6 +20,8 @@ const markdownInput = ref(null);
     v-model="markdown"
     spellcheck="false"
     class="w-full h-full resize-none px-3 py-2 outline-none border-t border-gray-200"
+    @input="setCursorPosition"
+    @click="setCursorPosition"
   >
   </textarea>
 </template>
--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -1,15 +1,30 @@
 <script setup>
 import markdownit from 'markdown-it'
+import markdownitAnchor from 'markdown-it-anchor'
+import markdownitDefList from 'markdown-it-deflist'
+import { full as markdownitEmoji } from 'markdown-it-emoji'
+import markdownitFootnote from 'markdown-it-footnote'
+import markdownitIns from 'markdown-it-ins'
+import markdownitSub from 'markdown-it-sub'
+import markdownitSup from 'markdown-it-sup'
+import markdownitTaskLists from 'markdown-it-task-lists'
 import { markdown } from '../stores/markdown'
 
-const md = markdownit();
-const markdownRender = md.render(markdown.value);
+const md = markdownit()
+  .use(markdownitAnchor)
+  .use(markdownitDefList)
+  .use(markdownitEmoji)
+  .use(markdownitFootnote)
+  .use(markdownitIns)
+  .use(markdownitSub)
+  .use(markdownitSup)
+  .use(markdownitTaskLists);
 </script>
 
 <template>
   <div
-    v-html="markdownRender"
-    class="prose px-3 py-2"
+    v-html="md.render(markdown)"
+    class="prose max-w-none h-full px-3 py-2 overflow-y-scroll border-t border-gray-200"
   >
   </div>
 </template>
